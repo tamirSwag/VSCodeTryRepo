c0ac5983e530c3461e14c8a4ec5a1eb8ef3a460e
TamirOfir <TamirOfir>
2016-08-24 21:41:52 +0300

    Commit

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert('App!');
+alert('The Best App!');
+
+function SayHay() {
+    console.log("Saying hay...");
+    alert("Hay!");
+    console.log('Said hay!');
+}
+
+alert("function is about to run");
+SayHay();
