0f377664a2e152437c5e1660fdb561c33f4503cb
TamirOfir <TamirOfir>
2016-08-24 21:45:06 +0300

    Staging

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-alert('The Best App!');
-
 function SayHay() {
     console.log("Saying hay...");
     alert("Hay!");
-    console.log('Said hay!');
+    console.log('"Hay" said');
 }
 
 alert("function is about to run");
